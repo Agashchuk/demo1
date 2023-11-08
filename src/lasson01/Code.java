a5c7d6d95111da753196b9ec2250197612c71ed3
anast <agachshuk1992@mail.ru>
2023-11-08 22:07:41 +0100

    lassen1

---
+++ src/lasson01/Code.java
@@ -0,0 +1,7 @@
+package lasson01;
+
+public class Code {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/lasson01/Hay.java
@@ -0,0 +1,7 @@
+package lasson01;
+
+public class Hay {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
